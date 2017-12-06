f107de550d184ce95756a08d7a83e5586cb5ab71
Ceesay Fofana <cfofana51@muleriders.saumag.edu>
2017-12-05 22:53:15 -0800

    Added functions to change, delete and display todos

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
 
-var todos = ["item 1", "item 2", "item 3"];
 
-function displayTodo(){
-    console.log(todos);
-}
 
-function addTodo(todo){
-   todos.push(todo); 
-}
 
-function changeTodo(position, newValue){
-    todos[position] = newValue;
-}
 
-function deleteTodo(position){
-    todos.splice(position, 1);
-}
+var todoList = {
+  
+  todos : [],
+  displayTodos : function(){
+    if(this.todos.length === 0){
+      console.log("Your todo list is empty!");
+    }else{
+      console.log("My Todos : ");
+      for (var i=0; i<this.todos.length; i++){
+      if(this.todos[i].completed === true){
+        console.log("(x)", this.todos[i].todoText);
+      }else{
+        console.log("( )", this.todos[i].todoText);
+        }
+      }
+    }
+  },
+  addTodo : function(todoText){
+    this.todos.push({
+      todoText : todoText,
+      completed : false
+    });
+    this.displayTodos();
+  },
+  changeTodo : function(position, todoText){
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo : function(position){
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted : function(position){
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+};
